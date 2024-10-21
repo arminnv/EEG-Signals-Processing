5c62bca86b9177fcbc9fafd666265dea98a15ee5
arminnv <76727063+arminnv@users.noreply.github.com>
2024-10-21 18:40:34 +0330

    Delete EEG_CHW2 directory

--- EEG_CHW2/Ex3/plotEEG.m
+++
@@ -1,9 +0,0 @@
-X  % 32-channel data
-load('Electrodes') ;
-
-% Plot Data
-% Use function disp_eeg(X,offset,feq,ElecName)
-offset = max(abs(X(:))) ;
-feq = 250 ;
-ElecName = Electrodes.labels ;
-disp_eeg(X,offset,feq,ElecName);
